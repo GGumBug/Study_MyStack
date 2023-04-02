7daa7183861128811ca0364082862c0aa6a336e8
GGumBug <alsrl7538@gmail.com>
2023-04-02 15:48:11 +0900

    Mystack

---
+++ MySttack/MySttack.cs
@@ -0,0 +1,57 @@
+﻿using System;
+namespace MySttack
+{
+	public class MySttack<T>
+	{
+		private T[] elements;
+		private int rear;
+
+		public MySttack(int size)
+		{
+			elements = new T[size];
+			rear = -1;
+		}
+
+		public void Push(T item)
+		{
+			if(rear >= elements.Length)
+			{
+				Console.WriteLine("Sttack overflow");
+				return;
+			}
+
+			elements[++rear] = item;
+		}
+
+		public T Pop()
+		{
+			if (rear < 0)
+			{
+				Console.WriteLine("Sttack underflow");
+				return default(T);
+			}
+
+			T itme = elements[rear];
+			elements[rear--] = default(T);
+
+			return itme;
+
+        }
+
+		public void Print()
+		{
+			if (rear < 0)
+			{
+				Console.WriteLine("Sttack is Empty");
+				return;
+			}
+
+			for (int i = 0; i < elements.Length; i++)
+			{
+				Console.Write($"{elements[i]} ");
+			}
+			Console.WriteLine();
+		}
+	}
+}
+
---
+++ MySttack/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace MySttack
+{
+	public class Program
+	{
+		static void Main(string[] args)
+		{
+			MySttack<int> mySttack = new MySttack<int>(5);
+
+			mySttack.Push(1);
+            mySttack.Push(2);
+            mySttack.Push(3);
+            mySttack.Push(4);
+            mySttack.Push(5);
+			mySttack.Pop();
+            mySttack.Pop();
+            mySttack.Pop();
+            mySttack.Pop();
+            mySttack.Push(5);
+            mySttack.Push(5);
+            mySttack.Push(5);
+
+            mySttack.Print();
+        }
+	}
+}
+
